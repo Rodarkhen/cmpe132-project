d01da96e7dcd43ad1064945dd46e0c44f4a18392
Rodrigo Chen <rodrigo.chen@sjsu.edu>
2024-04-25 15:49:56 -0700

    Added Hashed Password SHA256 & basic role-based user view on dashboard

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,10 @@
+# forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo
+
+# Define possible roles
+ROLES = [('student', 'Student'), ('faculty', 'Faculty'), ('librarian', 'Librarian')]
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -8,7 +12,10 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    first_name = StringField('First Name', validators=[DataRequired(), Length(max=25)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(max=25)])
+    username = StringField('Username', validators=[DataRequired() ]) #, Length(min=4, max=25)])
+    password = PasswordField('Password', validators=[DataRequired() ]) #, Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    role = SelectField('Role', choices=ROLES, validators=[DataRequired()])
     submit = SubmitField('Register')
